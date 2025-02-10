5f6fa134d355e3997a32b9ff6381a6684cc0f9aa
Mary Mikos <marymikos@marys-mbp-2.dyndns.rice.edu>
2025-02-10 14:48:02 -0600

    Initial commit at calibrating the sensor; further work is required to determine the conversion factor for inches and centimeters

--- Sensor.ino/Sensor.ino.ino
+++ Sensor.ino/Sensor.ino.ino
@@ -1,9 +1,34 @@
+const int trigPin = 11;
+const int echoPin = 12;
+long duration;
+int distanceCm, distanceInch;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // calculating the duration
+  duration = pulseIn(echoPin, HIGH);
 
+  // using the duration to calculate the sensor
+  distanceCm = duration * 100;
+  distanceInch = duration * 100;
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+  delay(1000);
 }
